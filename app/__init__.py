739e806108c6661010f1a80ba1bb6ea532c0d1a6
holelung <junho335276@gmail.com>
2024-10-31 10:17:16 +0900

    대충 수정하던거

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from dotenv import load_dotenv
+from app.db import db
+from app.models import *
+from app.config import Config
 
 # 환경 변수 로드
 load_dotenv()
@@ -7,6 +10,8 @@ load_dotenv()
 # Flask 앱 초기화 및 설정 함수
 def create_app():
     app = Flask(__name__)
+    
+    db.init_app(app)
 
     # 블루프린트 등록
     from app.routes import blueprints
