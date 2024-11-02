5ba3e572bb3568e25ff07f602bea27c81a14bad0
장준호 <jangjunho@jangjunhoui-MacBookPro.local>
2024-11-03 00:23:34 +0900

    sidebar 메뉴 수정

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,6 +1,7 @@
 // app/static/js/script.js
 document.addEventListener("DOMContentLoaded", function () {
-    const profileMenu = document.getElementById("profile-menu");
+    const profileMenu = document.getElementById("profileMenu");
+    profileMenu.classList.toggle("hidden");
 });
 function toggleMenu() {
     const menu = document.getElementById("profileMenu");
