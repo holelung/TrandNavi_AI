2745532d622947f19b0b72209f239321088eb8af
장준호 <jangjunho@jangjunhoui-MacBookPro.local>
2024-11-05 17:08:30 +0900

    화면 생성

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -6,4 +6,20 @@ main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def home():
-    return render_template('chat.html')  # chat.html 템플릿 파일을 사용할 경우
+    return render_template('main.html')  # chat.html 템플릿 파일을 사용할 경우
+
+@main_bp.route('/login')
+def login():
+    return render_template('login.html')  
+
+@main_bp.route('/register')
+def register():
+    return render_template('register.html')
+
+@main_bp.route('/cart') 
+def cart():
+    return render_template('cart.html')
+
+@main_bp.route('/chat') 
+def main():
+    return render_template('chat.html')
