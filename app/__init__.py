9dabd401d11d56b0f4aa7023eb9d9010855658fa
lewoo-ai <dldntjr100@naver.com>
2024-11-05 16:22:59 +0900

    llm

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,17 @@
-# app/__init__.py
 from flask import Flask
+from dotenv import load_dotenv
+#from app.db import db
+from app.models import *
 from app.config import Config
-from app.db import engine  # 생성된 engine 사용
-from app.models import Base  # Base 임포트
+
+# 환경 변수 로드
+load_dotenv()
 
 # Flask 앱 초기화 및 설정 함수
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)  # config 설정 불러오기
-
-    # 데이터베이스 테이블 생성
-    Base.metadata.create_all(engine)  # 엔진을 사용해 테이블 생성
+    
+    #db.init_app(app)
 
     # 블루프린트 등록
     from app.routes import blueprints
