4379e379f7c020b8aa8e70e3e72c71ef5b50578b
holelung <junho335276@gmail.com>
2024-12-10 00:02:48 +0900

    chat/roomid/message route 수정

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from flask import Flask, jsonify
 from app.config import Config
 from app.db import engine  # 생성된 engine 사용
 from app.models import Base  # Base 임포트
+
 from flask_jwt_extended import JWTManager
+from flask_jwt_extended import jwt_required
+
 from app.db.redis_client import redis_client
 from app.utils.token_utils import is_token_blacklisted
 from flask_cors import CORS
@@ -19,6 +22,11 @@ def create_app():
     # jwt 설정 
     jwt = JWTManager(app)
     
+    @app.route('/protected', methods=['GET'])
+    @jwt_required()
+    def protected():
+        return jsonify(message="This is a protected endpoint")
+    
     # 인증 오류 콜백 함수 설정
     @jwt.unauthorized_loader
     def unauthorized_response(callback):
@@ -26,11 +34,13 @@ def create_app():
             'message': 'Missing Authorization Header'
         }), 401
     
-    # 블랙리스트 검증 로직 추가
+    # 블랙리스트 검증 로직 추가    
     @jwt.token_in_blocklist_loader
-    def check_if_token_in_blacklist(jwt_header, jwt_payload):
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
         jti = jwt_payload["jti"]
-        return is_token_blacklisted(jti)    
+        token_in_blocklist = redis_client.get(jti)
+        return token_in_blocklist is not None
+
     
     # 데이터베이스 테이블 생성
     Base.metadata.create_all(engine)  # 엔진을 사용해 테이블 생성
