6f0014098e14bd7536e3b38edca38645aaa33e9d
holelung <junho335276@gmail.com>
2024-12-10 00:29:47 +0900

    product_url 용량수정

---
+++ migrations/versions/c767e7502f5a_cart_itemurl_byteup.py
@@ -0,0 +1,24 @@
+"""cart_itemurl_byteUP
+
+Revision ID: c767e7502f5a
+Revises: 9cf22b2a2372
+Create Date: 2024-12-10 00:29:19.948148
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c767e7502f5a'
+down_revision = '9cf22b2a2372'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
